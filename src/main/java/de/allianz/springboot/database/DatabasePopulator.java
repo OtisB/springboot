b57dff9f21e6dd7899c8ec3e189314a0784ff10f
Robin Schmidt <schmidt.rbn@gmail.com>
2023-03-27 12:24:30 +0200

    ADD dev, test and prod environment

--- src/main/java/de/allianz/springboot/database/DatabasePopulator.java
+++ src/main/java/de/allianz/springboot/database/DatabasePopulator.java
@@ -5,6 +5,7 @@ import de.allianz.springboot.repository.ToDoRepository;
 import de.allianz.springboot.service.ToDoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -35,14 +36,23 @@ public class DatabasePopulator implements CommandLineRunner {
     //LONGBOK DEPENDENCY INJECTION
     // WITH RequiredArgsConstructor ANNOTATION AND FINAL
     private final ToDoRepository toDoRepository;
+    private final ToDoService toDoService;
+    @Value("${environmentKey}")
+    public String environmentKey;
 
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println(environmentKey);
         final ToDo todo1 = new ToDo(null, "learn Java basics", "01.03.2023", true);
         final ToDo todo2 = new ToDo(null, "learn Spring", "21.03.2023", false);
         final ToDo todo3 = new ToDo(null, "Allianz", "01.04.2023", false);
 
-        toDoRepository.saveAll(List.of(todo1, todo2, todo3));
+        this.toDoService.createToDo(todo1);
+        this.toDoService.createToDo(todo2);
+        this.toDoService.createToDo(todo3);
+
+        //toDoRepository.saveAll(List.of(todo1, todo2, todo3));
+
     }
 }
